a9ac8f07bf1cc55ebb6feff01f575b3d648e55d8
Yuma Akhunza Kausar Putra <yuma.akhunza@gmail.com>
2025-03-11 07:52:50 +0700

    commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,14 +8,57 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index()
-    {
-        $data = [
-            'nama' => 'PelanggancPertama',
-        ];
-        UserModel::where('username', 'customer-1') ->update($data);
+    // public function index()
+    // {
+    //     $user = UserModel::all();
+    //     return view('user', ['data' => $user]);
+    // }
 
-        $user = UserModel::all();
+    // public function tambah()
+    // {
+    //     return view('user_tambah');
+    // }
+
+    // public function tambah_simpan(Request $request)
+    // {
+    //     UserModel::create([
+    //         'username' => $request->username,
+    //         'name' => $request->name,
+    //         'password' => Hash::make('$request->password'),
+    //         'level_id' => $request->level_id
+    //     ]);
+
+    //     return redirect('/user');
+    // }
+
+    // public function ubah($id)
+    // {
+    //     $user = UserModel::find($id);
+    //     return view('user_ubah', ['data' => $user]);
+    // }
+    // public function ubah_simpan($id, Request $request)
+    // {
+    //     $user = UserModel::find($id);
+
+    //     $user->username = $request->username;
+    //     $user->name = $request->name;
+    //     $user->password = Hash::make('$request->password');
+    //     $user->level_id - $request->level_id;
+
+    //     $user->save();
+
+    //     return redirect('/user');
+    // }
+    // public function hapus($id)
+    // {
+    //     $user = UserModel::find($id); 
+    //     $user->delete();
+
+    //     return redirect('/user');
+    // }
+    public function index()
+    {   
+        $user = UserModel::with('level')->get();
         return view('user', ['data' => $user]);
     }
 }
